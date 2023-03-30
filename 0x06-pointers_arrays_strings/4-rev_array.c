d361c6ba68772041acbddeaffcbf9ed440b6e70b
OPOKU-NUAMAH <francisopoku998@gmail.com>
2023-03-30 11:17:03 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,41 +1,21 @@
-#include "holberton.h"
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - check the code for Holberton School students.
- * @a: an array of integers
- * @n: the number of elements to swap
+ *reverse_array - revereses an array.
+ *@a: pointer to array.
+ *@n: number of elements of an array.
  *
- * Return: nothing.
+ *Return: void.
  */
-void print_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
-    int i;
+	int temp, counter;
 
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
+	n = n - 1;
+	counter = 0;
+	while (counter <= n)
+	{
+		temp = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temp;
+	}
 }
