f496c7a1fac5c9efdef80ba38a68f30ba8386db7
OPOKU-NUAMAH <francisopoku998@gmail.com>
2023-03-31 23:34:38 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,14 @@
-#include ''main.h''
+#include "main.h"
 /**
- * _isalpha - checks for char alphabet
- * @c: the char to be checked
- *
- * return: 1 if char is letter lower or upper, 0 otherwise
+ * _isalpha - Checks for alphabetic numbers
+ * @c : The character to be checked
+ * Return: 1 for alphabetic character or 0 for others.
  */
-int _isalpha.c(int c);
+int _isalpha(int c)
 {
-	if ((c > = 'a' && c < = 'z') || (c > = 'A' && < = 'Z'))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
-
