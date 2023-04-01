924692803ace6c33468e9e5ba2e5c0a8e7136d20
OPOKU-NUAMAH <francisopoku998@gmail.com>
2023-03-31 23:24:19 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,20 @@
-#include ''main.h''
+#include "main.h"
 /**
- * print print_alphabet_x10 - print 10 times alphabet, lowercase
+ * print_alphabet_x10 - Print 10x the alphabet
+ * Return :void
  */
-
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	char c;
+	int i = 0;
 
-	while (count++ <= 9)
+	while (i <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-
-			_print(letter);
-		_print('/n');
-	}
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+		}
+}
