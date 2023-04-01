926e67aa025ef34063298fa107e88e8265d8f10b
OPOKU-NUAMAH <francisopoku998@gmail.com>
2023-03-31 22:46:34 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main print with putchar
+ * Return 0
+ */
+int main(void)
+{
+
+        _putchar('a');
+	_putchar('\n');
+	_putchar('b');
+	_putchar('\n');
+
+	return (0);
+}
