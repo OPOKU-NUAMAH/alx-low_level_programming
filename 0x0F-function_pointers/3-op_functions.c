a4551c53aad62474350d02a3334eb17fe635033c
OPOKU-NUAMAH <francisopoku998@gmail.com>
2023-04-20 03:17:46 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,71 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
 /**
- * op_add - adds two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: add.
+ * get_op_func - matches operator from main
+ * @s: op str
+ * Return: 0
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-/**
- * op_sub - subctracts two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: difference.
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
+	int i = 0;
 
-/**
- * op_mul - multiplies two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: multiplication.
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - divides two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: division.
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	while (op_s[i].op)
 	{
-		printf("Error\n");
-		exit(100);
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
 	}
-	return (a / b);
-}
-
-/**
- * op_mod - calculates the module of two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: remainder of the division.
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+	return (NULL);
 }
