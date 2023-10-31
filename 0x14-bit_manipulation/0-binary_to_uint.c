8f835c38e25a07410a9ca5d615a6b0538e4a5e62
OPOKU-NUAMAH <francisopoku998@gmail.com>
2023-10-30 18:16:34 -0700

    0. 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,25 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+	unsigned int num = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (b[i + 1])
+	while (*b)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (*b < '0' || *b > '1')
 			return (0);
-		i++;
+		num <<= 1;
+		num += *b++ - '0';
 	}
 
-	while (i >= 0)
-	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
-
-	return (res);
-
+	return (num);
 }
